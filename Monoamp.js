316cf8e3b2d39ca1027190212ab10b4e13cf984a
mharrow <mharrow@tbaytel.net>
2016-04-13 23:58:21 -0400

    Parse Query response into variables values for HTML elements. Code parses request of one zone Not all zones for test purposes.

--- Monoamp.js
+++ Monoamp.js
@@ -11,6 +11,14 @@ var sendQuery = "?";
 var powerOn=false;
 var stringCmd="";
 var zoneValid=false;
+var Power_value="00";
+var Mute_value="00";
+var Bass_value="07";
+var Treble_value="07";
+var Balance_value="10";
+var Volume_value="20";
+var Source_value="01";
+var resp="";
 
 function run(){
 	// on load of window set zoneSelect to first zone and query status
@@ -22,7 +30,7 @@ function run(){
 	console.log("App start - Query status Zone 1:"+stringCmd);
 	serCmd(stringCmd);	// POST serial request for zone data
 	// all settings  first zone and power state of zone
-}
+	}
 
 function serCmd(stringCmd){
 	// call ajax data = stringCmd to be sent out the serial port
@@ -33,17 +41,31 @@ $.ajax({
 	data: ({serStr: stringCmd}),
 	//contentType: "application/json; charset=utf-8",
 	//dataType: "json",
-	success: function ( resp ){
-		console.log (resp);
-		//if ( resp == '#'){
-		//	console.log ("valid command acknowledged");
-		//	console.log("Reply is:"+resp);
-		//} else {
-		//	$( '.response' ).text ( 'failure' );
-		//	console.log("Reply is:"+resp);
-		// }
+	success: function (resp){
+		var n = resp.length;
+		console.log ("Reply is:"+resp);
+		console.log ("Reply length is:"+n);
+		if (n == 31){
+			Power_value = resp.substr(9,2);
+			Mute_value = resp.substr(11,2);
+			Volume_value = resp.substr(15,2);
+			Treble_value = resp.substr(17,2);
+			Bass_value = resp.substr(19,2);
+			Balance_value = resp.substr(21,2);
+			Source_value = resp.substr(23,2);
+			console.log ("PR value is:"+Power_value);
+			console.log ("MU value is:"+Mute_value);
+			console.log ("VO value is:"+Volume_value);
+			console.log ("TR value is:"+Treble_value);
+			console.log ("BS value is:"+Bass_value);
+			console.log ("BL value is:"+Balance_value);
+			console.log ("CH value is:"+Source_value);
+		}
 	}
+	
 });
+	
+	
 }
 
 function doAssignZone(){
