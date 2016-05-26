9e4eb04e39569a54a924b230f927d558ce4ac81e
mharrow <mharrow@tbaytel.net>
2016-05-25 20:30:12 -0400

    2nd Commit - php file madAmpMySqlUpdate.php changes

--- Server/madAmpMySqlUpdate.php
+++ Server/madAmpMySqlUpdate.php
@@ -7,20 +7,18 @@ header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
 // json from javascript client
 $command = json_decode(file_get_contents('php://input'), true);
 
-$table = $command[1];
-$field = $command[2];
-$fieldValue = $command[3];
-$pk = $command[4];
-$pkValue = $command[5];
+$table = $command[0];
+$field = $command[1];
+$fieldValue = $command[2];
+$pk = $command[3];
+$pkValue = $command[4];
 
-$str = "python Drivers/my_sql_update.py " . "'" . $table. " ' " . "'"$field "' ". "'"$fieldValue" ' " . "'"$pk" ' " . "'"$pkValue"'"
 // python argument must be in quotes 
-// $str = "python Drivers/my_sql_update.py " . chr(39) . $command['serStr'] . chr(39);
+$str = sprintf("python Drivers/my_sql_update.py " . "'" . $table. " ' " . "'"$field "' ". "'"$fieldValue" ' " . "'"$pk" ' " . "'"$pkValue"'");
 
 exec($str, $output);
 
 //echo array as string back to javascript - ajax call response
 echo $output[0];
-
 
 ?>
